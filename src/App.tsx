6aff71a2643456163741b800c9a4ecb50fa26f70
vandoors <143453791+vandoors@users.noreply.github.com>
2024-02-06 13:30:07 -0600

    More ui component testing

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,17 @@ import './App.css';
 function App() {
   return (
       <div className="App">
-        <button className="btn btn-primary">Primary</button>
+          <button className="btn btn-primary">Primary</button>
+
+          <div className="card w-96 bg-base-100 shadow-md">
+              <div className="card-body">
+                  <h2 className="card-title">Card title!</h2>
+                  <p>If a dog chews shoes whose shoes does he choose?</p>
+                  <div className="card-actions justify-end">
+                      <button className="btn btn-primary">Buy Now</button>
+                  </div>
+              </div>
+          </div>
       </div>
   );
 }
