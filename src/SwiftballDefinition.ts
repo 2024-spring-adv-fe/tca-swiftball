ae5007ff46ade221b96e4ed6b417b82e5de65784
vandoors <143453791+vandoors@users.noreply.github.com>
2024-02-15 13:01:55 -0600

    export LeaderboardEntry
    to be modified for my needs later

--- src/SwiftballDefinition.ts
+++ src/SwiftballDefinition.ts
@@ -2,3 +2,10 @@ export type SwiftballBallot = {
     player: string;
     guesses: string[];
 };
+
+export type LeaderboardEntry = {
+    wins: number;
+    losses: number;
+    avg: number;
+    name: string;
+};
